7f7c65a4a8f5f20ae1fa9068614b5ffa9fcfab1a
Peter Vizi <peter.vizi@gmail.com>
2020-02-03 15:22:31 -0500

    Set up the hostname

--- setup.sh
+++ setup.sh
@@ -6,4 +6,10 @@ KEY_ID=6C72CD1959DF7DEC
 gpg --recv ${KEY_ID}
 gpg --armor --export ${KEY_ID} | sudo apt-key add
 
-echo "deb https://s3-us-west-1.amazonaws.com/repo.socialtag.tv/beta/ vankman main" | sudo tee -a /etc/apt/sources.list.d/socialtag.list
+echo "deb https://s3-us-west-1.amazonaws.com/repo.socialtag.tv/beta/ vankman main" > /etc/apt/sources.list.d/socialtag.list
+apt-get -y update
+apt-get -y install socialtag-raspberry-system
+
+THE_NEW_HOSTNAME=$(cat /sys/class/net/eth0/address | tr -d ':')
+echo ${THE_NEW_HOSTNAME} > /etc/hostname
+echo "127.0.0.1 ${THE_NEW_HOSTNAME}" >> /etc/hosts
