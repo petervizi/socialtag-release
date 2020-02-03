283122402326711e0a03e7f5512eb1f9e2cfac4c
Peter Vizi <peter.vizi@gmail.com>
2020-02-03 15:47:50 -0500

    bas64 encode the patch

--- setup.sh
+++ setup.sh
@@ -16,18 +16,15 @@ echo "127.0.0.1 ${THE_NEW_HOSTNAME}" >> /etc/hosts
 
 systemctl disable wpa_supplicant.service
 
-cat <<EOF > 10-wpa_supplicant.patch
---- 10-wpa_supplicant	2019-12-09 05:26:29.943407687 -1000
-+++ /lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant	2019-11-30 06:21:11.360382338 -1000
-@@ -59,7 +59,7 @@
- 	syslog info "starting wpa_supplicant"
- 	wpa_supplicant_driver="${wpa_supplicant_driver:-nl80211,wext}"
- 	driver=${wpa_supplicant_driver:+-D}$wpa_supplicant_driver
--	err=$(wpa_supplicant -B -c"$wpa_supplicant_conf" -i"$interface" \
-+	err=$(wpa_supplicant -u -B -c"$wpa_supplicant_conf" -i"$interface" \
- 	    "$driver" 2>&1)
- 	errn=$?
- 	if [ $errn != 0 ]; then
-  EOF
+echo "LS0tIDEwLXdwYV9zdXBwbGljYW50CTIwMTktMTItMDkgMDU6MjY6MjkuOTQzNDA3Njg3IC0xMDAw
+CisrKyAvbGliL2RoY3BjZC9kaGNwY2QtaG9va3MvMTAtd3BhX3N1cHBsaWNhbnQJMjAxOS0xMS0z
+MCAwNjoyMToxMS4zNjAzODIzMzggLTEwMDAKQEAgLTU5LDcgKzU5LDcgQEAKIAlzeXNsb2cgaW5m
+byAic3RhcnRpbmcgd3BhX3N1cHBsaWNhbnQiCiAJd3BhX3N1cHBsaWNhbnRfZHJpdmVyPSIke3dw
+YV9zdXBwbGljYW50X2RyaXZlcjotbmw4MDIxMSx3ZXh0fSIKIAlkcml2ZXI9JHt3cGFfc3VwcGxp
+Y2FudF9kcml2ZXI6Ky1EfSR3cGFfc3VwcGxpY2FudF9kcml2ZXIKLQllcnI9JCh3cGFfc3VwcGxp
+Y2FudCAtQiAtYyIkd3BhX3N1cHBsaWNhbnRfY29uZiIgLWkiJGludGVyZmFjZSIgXAorCWVycj0k
+KHdwYV9zdXBwbGljYW50IC11IC1CIC1jIiR3cGFfc3VwcGxpY2FudF9jb25mIiAtaSIkaW50ZXJm
+YWNlIiBcCiAJICAgICIkZHJpdmVyIiAyPiYxKQogCWVycm49JD8KIAlpZiBbICRlcnJuICE9IDAg
+XTsgdGhlbgo=" | base64 --decode > 10-wpa_supplicant.patch
 
-  patch < 10-wpa_supplicant.patch
+patch < 10-wpa_supplicant.patch
